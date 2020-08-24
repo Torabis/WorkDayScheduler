8d177129b9734533cae5d06b8bbf47765efa8d43
Torabis <sadihome@gmail.com>
2020-08-23 23:07:18 -0700

    saved events persist

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,53 @@
-// set current dat at the top
-let curDay = moment().format("dddd, MMMM Do, YYYY h:mm a");
-let curTime = moment().format("H");
-let curTimeNum = parseInt(curTime);
+$(document).ready(function () {
 
-// $("row-9").attr("data-time", moment("9:00 am", "h:mm a").format("H"));
-// $("row-10").attr("data-time", moment("10:00 am", "h:mm a").format("H"));
-// $("row-11").attr("data-time", moment("11:00 am", "h:mm a").format("H"));
-// $("row-12").attr("data-time", moment("12:00 pm", "h:mm a").format("H"));
-// $("row-1").attr("data-time", moment("1:00 pm", "h:mm a").format("H"));
-// $("row-2").attr("data-time", moment("2:00 pm", "h:mm a").format("H"));
-// $("row-3").attr("data-time", moment("3:00 pm", "h:mm a").format("H"));
-// $("row-4").attr("data-time", moment("4:00 pm", "h:mm a").format("H"));
-// $("row-5").attr("data-time", moment("5:00 pm", "h:mm a").format("H"));
-
-// indicate past/present/future color for each time block
-for (var i = 1; i <= 17; i++) {
-
-  let blTimeNum = parseInt(i);
+    var currentDay = moment().format("dddd, MMMM Do");
+  $("#currentDay").text(currentDay);
   
-  if (curTimeNum === blTimeNum) {
-    $("#" + "row-" + i ).addClass("present");
+    // set current dat at the top
+  let curDay = moment().format("dddd, MMMM Do, YYYY h:mm a");
+
+  let curTime = moment().format("H");
+  let curTimeNum = parseInt(curTime);
+
+  // indicate past/present/future color for each time block
+  for (var i = 1; i <= 17; i++) {
+    let blTimeNum = parseInt(i);
+    let blockID = "#" + "row-" + i;
+    if (curTimeNum === blTimeNum) {
+      $(blockID).addClass("present");
+    }
+    if (curTimeNum > blTimeNum) {
+      $(blockID).addClass("past");
+    }
+    if (curTimeNum < blTimeNum) {
+      $(blockID).addClass("future");
+    }
   }
-  if (curTimeNum > blTimeNum) {
-    $("#" + "row-" + i ).addClass("past");
-  }
-  if (curTimeNum < blTimeNum) {
-    $("#" + "row-" + i ).addClass("future");
+
+  // call the function to populate the input values with stored data
+  storPlans();
+
+  // save button function
+  var saveBtn = $(".saveBtn");
+
+  saveBtn.on("mouseenter", function () {
+    $(this).addClass("hover");
+  });
+
+  saveBtn.on("mouseleave", function () {
+    $(this).removeClass("hover");
+  });
+
+  // save button store & retrieve data / local storage
+  saveBtn.on("click", function () {
+    var saveHr = $(this).attr("save-hr");
+    var toDo = $("#" + "row-" + saveHr).val();
+    localStorage.setItem(saveHr, toDo);
+  });
+});
+
+function storPlans() {
+  for (var i = 1; i <= 12; i++) {
+    $("#" + "row-" + i).val(localStorage.getItem(i));
   }
 }
-
-
